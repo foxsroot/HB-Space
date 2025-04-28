16b5f88928e3f1fd69706d719c6a30f81611c5f5
Jer <rickoaxeldinata@gmail.com>
2025-04-28 11:35:38 +0700

    Revised Post Model

--- backend/models/Post.ts
+++ backend/models/Post.ts
@@ -1,13 +1,14 @@
-import { Table, Model, Column, DataType } from 'sequelize-typescript';
+import { Table, Model, Column, DataType, ForeignKey, BelongsTo } from 'sequelize-typescript';
+import { User } from "./User";
 
 @Table({
-    tableName: "users",
+    tableName: "posts",
     timestamps: false,
-    createdAt: "uploaded_at",
-    updatedAt: "edited_at"
+    createdAt: "created_at",
+    updatedAt: "updated_at"
 })
 
-export class User extends Model {
+export class Post extends Model {
     @Column({
         type: DataType.UUID,
         primaryKey: true,
@@ -22,11 +23,22 @@ export class User extends Model {
         allowNull: false,
         field: "image_file_path"
     })
-    declare imageFile: string;
+    declare image: string;
 
     @Column({
         type: DataType.STRING,
         allowNull: false
     })
     declare caption: string;
+
+    @ForeignKey(() => User)
+    @Column({
+        type: DataType.UUID,
+        field: "user_id",
+        allowNull: false
+    })
+    declare userId: string;
+
+    @BelongsTo(() => User)
+    declare user: User;
 }
