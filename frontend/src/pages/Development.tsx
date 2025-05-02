4a97218a0aaa67d6ce55ff6a239305390ea012b4
Jer <rickoaxeldinata@gmail.com>
2025-05-02 13:08:43 +0700

    Added User List Dialog

--- frontend/src/pages/Development.tsx
+++ frontend/src/pages/Development.tsx
@@ -1,28 +1,41 @@
-// import { useState } from "react";
-// import AlertBox from "../components/AlertBox";
-import Post from "../components/PostPreview.tsx";
+import { useState } from "react";
+import { Button } from "@mui/material";
+import UserList from "../components/UserList";
 
 const Development = () => {
-  //   const [alertVisibility, setAlertVisibility] = useState(true);
-  //   return (
-  //     alertVisibility && (
-  //       <AlertBox
-  //         severity="error"
-  //         variant="filled"
-  //         onClose={() => setAlertVisibility(false)}
-  //       >
-  //         Error Creating User Account
-  //       </AlertBox>
-  //     )
-  //   );
+  const [open, setOpen] = useState(false);
+  const [users, setUsers] = useState([
+    { id: "1", username: "user1", isFollowing: false },
+    { id: "2", username: "user2", isFollowing: true },
+    { id: "3", username: "user3", isFollowing: false },
+    { id: "4", username: "user1", isFollowing: false },
+    { id: "5", username: "user2", isFollowing: true },
+    { id: "6", username: "user3", isFollowing: false },
+    { id: "7", username: "user1", isFollowing: false },
+    { id: "8", username: "user2", isFollowing: true },
+    { id: "9", username: "user3", isFollowing: false },
+  ]);
+
+  const handleFollowToggle = (userId: string) => {
+    setUsers((prevUsers) =>
+      prevUsers.map((user) =>
+        user.id === userId ? { ...user, isFollowing: !user.isFollowing } : user
+      )
+    );
+  };
 
   return (
-    <Post
-      image="/dummies/Post.png"
-      alt="Sample Post"
-      likes={42}
-      postId="abc123"
-    />
+    <div style={{ padding: 24 }}>
+      <Button variant="contained" onClick={() => setOpen(true)}>
+        Show Users
+      </Button>
+      <UserList
+        open={open}
+        onClose={() => setOpen(false)}
+        users={users}
+        onFollowToggle={handleFollowToggle}
+      />
+    </div>
   );
 };
 
